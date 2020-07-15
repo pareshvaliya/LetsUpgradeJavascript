68048f5b2280b7c61a5aec64a1fa9aca1ae3af5f
pareshvaliya <65116001+pareshvaliya@users.noreply.github.com>
2020-07-15 12:08:42 +0530

    third question

---
+++ Day1/third.js
@@ -0,0 +1,30 @@
+// Datatypes in JavaScripts
+
+// Primitive Datatype
+/*
+string
+number
+boolean
+undefined
+*/
+
+var length = 16;   // Number can be with decimal or without decimal
+var lastName = "Valiya"; // String
+var x; // undefined
+var bool = true // boolean
+console.log(length,lastName,x,bool);
+
+// Complex Datatype
+/*
+function
+object
+*/
+
+var detail = {'name':'Paresh','age':24} // object
+console.log(detail);
+
+function myfirst()
+{
+    console.log("firstfunction");
+}
+myfirst(); // function
